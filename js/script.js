fcf10cb90fa56d0be12925a72b509fc696b535d5
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-05-03 17:30:21 +0200

    created

--- js/script.js
+++ js/script.js
@@ -21,7 +21,12 @@ console.log('Vue', Vue); // Verifica Vue
     // always executed
   });
  * 
+ *      Descrizione
  * 
+ *  Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista con Vue JS e Axios.
+ * 
+ *  Bonus
+ *  Far comparire gli indirizzi email solamente quando sono stati tutti generati.
  * 
  * 
  */
@@ -32,8 +37,29 @@ const app = new Vue({
     el: '#app',
 
     data: {
+        listaEmail:[],
+    },
+
+    created(){
+        // API CALL
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then( result =>{
+            // Success
+            console.log(result)
+            console.log(result.data)
+            console.log('EMail random: ', result.data.response)
+
+            this.listaEmail = result.data.response
+
+            console.log('lista email: ', listaEmail)
+        })
+        .catch(err =>{
+            console.log('Errore: ', err)
+        })
+    console.log(listaEmail)
 
     },
+    
 });
     
     
